057156bf13225d8fa28dbce116934f254ee07805
ameyd96 <erameydeshpande@gmail.com>
2021-10-25 01:04:16 -0700

    State management is done

--- 01-starting-setup/src/componenets/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/componenets/Expenses/ExpenseItem.js
@@ -1,20 +1,30 @@
+import React, {useState} from 'react';
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
 
+    const [title,setTitle] = useState(props.title);
+
+
   // const month = props.date.toLocaleString('en-US',{ month: 'long'});
   // const day = props.date.toLocaleString('en-US',{day: '2-digit'});
   // const year = props.date.getFullYear();
 
+    const clickedHandler = ()=>{
+      setTitle('Updated');
+      console.log('clicked...')
+    }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickedHandler}>Change title</button>
     </Card>
   );
 };
