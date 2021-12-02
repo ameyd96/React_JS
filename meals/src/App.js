fcd3ef19240d121fcbc7486ebb12e68c780e4b69
ameyd96 <erameydeshpande@gmail.com>
2021-12-01 22:55:23 -0800

    meals component is added

--- meals/src/App.js
+++ meals/src/App.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from "react";
 
 import Header from "./component/Layout/Header";
-
+import Meals from "./component/Meals/Meals";
 const App = () => {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 };
