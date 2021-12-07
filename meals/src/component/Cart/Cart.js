cacfc2aadb51e56bf9118f0b3e0c9a6f401013a8
ameyd96 <erameydeshpande@gmail.com>
2021-12-07 00:32:39 -0800

    removing meal amount from cart is done

--- meals/src/component/Cart/Cart.js
+++ meals/src/component/Cart/Cart.js
@@ -11,8 +11,12 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemovehandler = (id) => {};
-  const cartItemAddHandler = (item) => {};
+  const cartItemRemovehandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item,amount:1})
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
