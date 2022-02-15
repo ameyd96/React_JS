c139eaf777d3c6f31407ec146180566294bc41ee
ameyd96 <erameydeshpande@gmail.com>
2022-02-14 22:42:40 -0800

    Outputting quotelist data

--- Quotes/src/pages/AllQuotes.js
+++ Quotes/src/pages/AllQuotes.js
@@ -1,6 +1,14 @@
-const AllQuotes = () => {
-    return <h1>All Quotes</h1>;
-};
+import QuoteList from "../components/quotes/QuoteList";
 
+const DUMMY_DATA = [
+  { id: "1", author: "Max", text: "Learning React" },
+  { id: "2", author: "Mini", text: "Learning Java" },
+  { id: "3", author: "Pro", text: "Learning Spring" },
+  { id: "4", author: "Sam", text: "Learning JS" },
+];
+
+const AllQuotes = () => {
+  return <QuoteList quotes={DUMMY_DATA} />;
+};
 
 export default AllQuotes;
