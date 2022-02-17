5dd25aec0bece54bc04fd478f698df56b1ff4dd6
ameyd96 <erameydeshpande@gmail.com>
2022-02-17 02:44:19 -0800

    all quotes are fetched from firebase

--- Quotes/src/pages/AllQuotes.js
+++ Quotes/src/pages/AllQuotes.js
@@ -1,14 +1,48 @@
-import QuoteList from "../components/quotes/QuoteList";
+import { useEffect } from "react";
 
-const DUMMY_DATA = [
-  { id: "1", author: "Max", text: "Learning React" },
-  { id: "2", author: "Mini", text: "Learning Java" },
-  { id: "3", author: "Pro", text: "Learning Spring" },
-  { id: "4", author: "Sam", text: "Learning JS" },
-];
+import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+import NoQuotesFound from '../components/quotes/NoQuotesFound'
+
+// const DUMMY_DATA = [
+//   { id: "1", author: "Max", text: "Learning React" },
+//   { id: "2", author: "Mini", text: "Learning Java" },
+//   { id: "3", author: "Pro", text: "Learning Spring" },
+//   { id: "4", author: "Sam", text: "Learning JS" },
+// ];
 
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_DATA} />;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if(error){
+    return <p className="centered focused">{error}</p>
+  }
+
+
+  if(status === 'completed' && (!loadedQuotes || loadedQuotes.length ===0)){
+    return <NoQuotesFound />
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
