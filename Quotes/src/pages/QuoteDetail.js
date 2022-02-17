be8764960c850d4d286679a1d94fec4c5823900e
ameyd96 <erameydeshpande@gmail.com>
2022-02-17 03:21:18 -0800

    loading detail of quotes from firebase

--- Quotes/src/pages/QuoteDetail.js
+++ Quotes/src/pages/QuoteDetail.js
@@ -1,27 +1,57 @@
-import { Route, useParams, Link,useRouteMatch } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
-const DUMMY_DATA = [
-  { id: "1", author: "Max", text: "Learning React" },
-  { id: "2", author: "Mini", text: "Learning Java" },
-  { id: "3", author: "Pro", text: "Learning Spring" },
-  { id: "4", author: "Sam", text: "Learning JS" },
-];
+// const DUMMY_DATA = [
+//   { id: "1", author: "Max", text: "Learning React" },
+//   { id: "2", author: "Mini", text: "Learning Java" },
+//   { id: "3", author: "Pro", text: "Learning Spring" },
+//   { id: "4", author: "Sam", text: "Learning JS" },
+// ];
 
 const QuoteDetail = () => {
-  const match =useRouteMatch();
+
+
+  const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getSingleQuote,true);
+  const { quoteId } = params;
 
-  if (!quote) {
+  // const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (!loadedQuotes.text) {
     return <p>No Quote Found</p>;
   }
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuotes.text} author={loadedQuotes.author} />
       <Route path={`${match.path}`} exact>
         <div className="centered">
           <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
