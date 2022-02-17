50fca96c6edbb5758c5d362706badd8f0e536156
ameyd96 <erameydeshpande@gmail.com>
2022-02-17 02:32:13 -0800

    new quotes are added to firebase using http

--- Quotes/src/lib/api.js
+++ Quotes/src/lib/api.js
@@ -1,4 +1,5 @@
-const FIREBASE_DOMAIN = 'https://react-prep-default-rtdb.firebaseio.com';
+const FIREBASE_DOMAIN = 'https://httproute-2bfe7-default-rtdb.firebaseio.com';
+
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
--- Quotes/src/pages/NewQuote.js
+++ Quotes/src/pages/NewQuote.js
@@ -1,16 +1,27 @@
+import { useEffect } from "react";
+
 import { useHistory } from "react-router-dom";
 
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
   const history = useHistory();
 
-  const addQuotehandler = (quoteData) => {
-    console.log(quoteData);
+  const { sendRequest, status } = useHttp(addQuote);
 
-    history.push('/quotes');
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status,history]);
+
+  const addQuotehandler = (quoteData) => {
+    sendRequest(quoteData);
+    //history.push("/quotes");
   };
-  return <QuoteForm onAddQuote={addQuotehandler} />;
+  return <QuoteForm isLoading={status ==='pending'} onAddQuote={addQuotehandler} />;
 };
 
 export default NewQuote;
