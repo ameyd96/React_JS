06f390b84bc7abff096c705765194b1100c85454
ameyd96 <erameydeshpande@gmail.com>
2022-05-24 02:00:38 -0700

    paginatin is completed

--- pagination_pro/src/App.js
+++ pagination_pro/src/App.js
@@ -3,30 +3,31 @@ import ReactPaginate from "react-paginate";
 import "./App.css";
 
 function App() {
-   const url = "https://jsonplaceholder.typicode.com/comments?_page=1&_limit=12";
   // const url =
   //   "https://jsonplaceholder.typicode.com/comments?_page=${currentPage}&_limit=12";
   const [items, setItems] = useState([]);
   const [pageCount,setPageCount] = useState(0)
-
+  
+  let limit =12
   useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=${limit}`;
     const getComments = async () => {
       const res = await fetch(url);
       const data = await res.json(); 
       // to get total number of data coming from api
       const total = res.headers.get('x-total-count')
-      setPageCount(total/12)
-      console.log(total)
+      setPageCount(Math.ceil(total/limit))
+      //console.log(total)
       setItems(data);
     };
     getComments();
-  }, []);
+  }, [limit]);
 
-  console.log(items);
+ // console.log(items);
 
   const fecthComments = async (currentPage) => {
     const res = await fetch(
-      `https://jsonplaceholder.typicode.com/comments?_page=${currentPage}&_limit=12`
+      `https://jsonplaceholder.typicode.com/comments?_page=${currentPage}&_limit=${limit}`
     );
     const data = await res.json();
     return data
