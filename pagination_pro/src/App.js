eb45033ac7f0d2599bb76cc6663d4a3096e19428
ameyd96 <erameydeshpande@gmail.com>
2022-05-24 00:46:30 -0700

    pagination project is created

---
+++ pagination_pro/src/App.js
@@ -0,0 +1,14 @@
+import ReactPaginate from 'react-paginate';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ReactPaginate 
+      previousLabel={"previous"}
+      />
+    </div>
+  );
+}
+
+export default App;
